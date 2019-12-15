3a6c30f6d3fb60e21bf0a7724754281cc60205cc
sreenandini <mallelasreenandini2@gmail.com>
2019-12-15 23:40:36 +0000

    Drivers component grid is populated with stub data Wiring to the real API services for list , create , delete is pending Graphql schema , code generation and services are generated

--- src/app/drivers/drivers.component.ts
+++ src/app/drivers/drivers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {todos} from '../todos';
+import * as queries from '../../graphql/queries';
+import * as mutations from '../../graphql/mutations';
+import {API, graphqlOperation} from 'aws-amplify';
 
 @Component({
   selector: 'app-drivers',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drivers.component.scss']
 })
 export class DriversComponent implements OnInit {
-
-  constructor() { }
+todos
+  constructor() {
+  this.todos =  todos; //API.graphql(graphqlOperation(queries.listMyTypes));
+  }
 
   ngOnInit() {
+  }
+
+  createItem(todo){
+  //API.graphql(graphqlOperation(mutations.createTodo(todo));
+  }
+
+  deleteItem(){
+  //API.graphql(graphqlOperation(mutations.deleteTodo(todo));
   }
 
 }
